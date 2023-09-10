d8edbc9a0affecef93ec112e14affe035b1ac17f
Mickey4527 <saran.za097@gmail.com>
2023-09-11 01:22:34 +0700

    0.2.1

--- pages/setup/setup.php
+++ pages/setup/setup.php
@@ -1,23 +1,22 @@
 <?php
+include_once('../pages/setup/setup-views.php');
 
-  if(checkMySqliExp() === true && checkPHP($config['reqsys']['php']) === true){
-    if(!isset($config['db']['server']) || !isset($config['db']['user']) || !isset($config['db']['password']) || !isset($config['db']['database']) || $config['sys']['setup'] === true){
-        header("Location: ../views/setup.php");
-    } else {
-        $db = new database();
-        $db->setServername($config['db']['server']);
-        $db->setUsername($config['db']['user']);
-        $db->setPassword($config['db']['password']);
-        $db->setDbname($config['db']['database']);
-        $db->getconn();
-    }
-
+  if(!checkMySqliExp() || !checkPHP($config['reqsys']['php'])){
+    $header = 'Setup error';
+    $report = checkMySqliExp() ? "" : "<p>MySqli extension is not installed.</p><br>";
+    $report .= checkPHP($config['reqsys']['php']) ? "" : "<p>PHP version not support</p><br>";
+    header("Location: ../views/error.php?report=$report&header=$header");
+    exit();
+}
+  if(!isset($config['db']['server']) || !isset($config['db']['user']) || !isset($config['db']['password']) || !isset($config['db']['database']) || $config['sys']['setup'] === true){
+    header("Location: ../views/setup.php");
   } else {
-        $header = 'Setup error';
-        $report = checkMySqliExp() ? "" : "<p>MySqli extension is not installed.</p><br>";
-        $report .= checkPHP($config['reqsys']['php']) ? "" : "<p>PHP version not support</p><br>";
-        header("Location: ../views/error.php?report=$report&header=$header");
-
+    $db = new database();
+    $db->setServername($config['db']['server']);
+    $db->setUsername($config['db']['user']);
+    $db->setPassword($config['db']['password']);
+    $db->setDbname($config['db']['database']);
+    $db->getconn();
   }
 
 ?>
--- views/setup.php
+++ views/setup.php
@@ -1,9 +1,9 @@
 <?php
     include_once('../pages/auth.php');
     $config_header = "<style>html, body{background-color: #f1f1f1; height: 100%;}</style>";
+    include_once('../pages/setup/setup.php');
     include_once('../pages/header.php');
     include_once('../pages/modal/modal-box.php');
-    include_once('../pages/setup/setup-views.php');
 ?>
 <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <?php
