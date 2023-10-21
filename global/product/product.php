a64e5e7c4ba1b511ae064a450344b5c21ac978ca
thanawat88 <toon1072@hotmail.com>
2023-10-21 23:55:08 +0700

    doingeditproduct

--- global/product/product.php
+++ global/product/product.php
@@ -2,26 +2,13 @@
     include_once '../conn.php';
     include_once '../function.php';
 
+    
     if(isset($_POST['submitCreateProduct'])){
-        if(empty($_POST['ProductName']) || empty($_POST['Category']) || empty($_POST['Stock']) || empty($_POST['SellingPrice']) || empty($_POST['Dateadded']) ){
+        if(empty($_POST['ProductName']) || empty($_POST['Category']) || empty($_POST['Stock']) || empty($_POST['Quantityused']) || empty($_POST['Dateadded']) ){
             header('Location: ../../views/product_manager.php?error=emptyfields');
             exit();
         }
-        
-        else{
-            $ProductName = $_POST['ProductName'];
-            $Category = $_POST['Category'];
-            $Stock = $_POST['Stock'];
-            $SellingPrice = $_POST['SellingPrice'];
-            $Dateadded = $_POST['Dateadded'];
-            if(insertAnySql($conn,'product_manager','product_name,product_category,product_stock,product_price,date_added',"'$ProductName','$Category','$Stock','$SellingPrice','$Dateadded'")){
-                header('Location: ../../views/product_manager.php?success=insert');
-                exit();
-            }
-            else{
-                header('Location: ../../views/product_manager.php?error=insert');
-                exit();
-            }
-        }
     }
+        
+
 ?>
--- views/product_manager.php
+++ views/product_manager.php
@@ -34,16 +34,16 @@
                 </thead>
                 <tbody>
                     <?php
-                    $result = getAllSql($conn,'product_id,product_name,product_category,product_stock,product_Quantity,date_added','product_manager');
+                    $result = getAllSql($conn,'product_id,product_name,product_category,product_stock,product_quantity,date_added','product_manager');
                     foreach($result as $row){
                         echo '<tr>';
                         echo '<th scope="row">'.$row['product_id'].'</th>';
                         echo '<td>'.$row['product_name'].'</td>';
                         echo '<td>'.$row['product_category'].'</td>';
                         echo '<td>'.$row['product_stock'].'</td>';
-                        echo '<td>'.$row['product_Quantity'].'</td>';
+                        echo '<td>'.$row['product_quantity'].'</td>';
                         echo '<td>'.$row['date_added'].'</td>';
-                        echo '<td><a class="btn small" href="#"><i class="bi bi-pencil-square text-primary"></i>แก้ไขสินค้า</a><a class="btn small" href="#"><i class="bi bi-trash-fill text-primary"></i>ลบสินค้า</a></td>';
+                        echo '<td> <a class="btn small" href=""><i class="bi bi-pencil-square text-primary" data-bs-toggle="modal" data-bs-target="#Editpro"></i>แก้ไขสินค้า</a> <a class="btn small" href="#"><i class="bi bi-trash-fill text-primary"></i>ลบสินค้า</a></td>';
                         echo '</tr>';
                     }
                     ?>
@@ -108,6 +108,62 @@
     </div>
   </div>
 </div>
+
+<div class="modal" id="Editpro" tabindex="-1" aria-labelledby="EditproLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="EditproLabel">เพิ่มสินค้า</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="container p-3">
+            
+                <div class="row">
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label for="ProductName" class="form-label">ชื่อสินค้า</label>
+                            <input type="text" class="form-control" id="ProductName" placeholder="ชื่อสินค้า">
+                        </div>
+                    </div>
+                    <div class="col-6">
+                
+                        <label for="Category" class="form-label">ประเภทสินค้า</label>
+                        <select class="form-select" id="Category" aria-label="Floating label select example">
+                            <option selected>ประเภทสินค้า</option>
+                            <option value="1">Coffee</option>
+                            <option value="2">Tea</option>
+                        </select>
+                        </div>
+                    </div>
+
+                    <label for="Stock" class="form-label">จำนวน</label>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="จำนวน" aria-label="จำนวน" aria-describedby="Stock">
+                        <span class="input-group-text" id="Stock">ชอต/แก้ว</span>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="mb-3">
+                            <label for="Dateadded" class="form-label">วันที่เพิ่มสินค้า</label>
+                            <input name="Dateadded" type="date" class="form-control" id="date" placeholder="วันที่เพิ่มสินค้า">
+                            <div class="form-check mt-3">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    ตั้งเป็นวันที่ปัจจุบัน
+                                </label>
+                            </div>
+                </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
+        <button type="submit" class="btn btn-primary">เพิ่ม</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div> 
 
 <?php
     htmlFooter();
