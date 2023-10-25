cdccce9a0fe7ad82f256071eec9fff3280fc74c7
thanawat88 <toon1072@hotmail.com>
2023-10-25 23:55:30 +0700

    updatehome

--- views/home.php
+++ views/home.php
@@ -3,6 +3,32 @@
     include_once ('../global/header.php');
     htmlHeader('Home');
 ?>
-<a href="login.php">Login</a>
 
-<a href="login.php">Login</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="../assets/style-home.css">
+</head>
+<body>
+    <section>
+        <header>
+            <a href="#" class="logo"><img src="../source/img/home/logo.png"></a>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+        </header>
+    </section>
+
+    <a href="login.php">Login</a>
+
+    
+    
+</body>
+</html>
+
+
+
