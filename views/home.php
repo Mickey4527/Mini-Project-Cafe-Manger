832869705616966b09d844fff0024a3ad1bdb89d
thanawat88 <toon1072@hotmail.com>
2023-10-28 02:58:26 +0700

    commit home

--- views/home.php
+++ views/home.php
@@ -10,25 +10,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="../assets/style-home.css">
+    <link rel="stylesheet" href="../assets/css/style-home.css">
 </head>
 <body>
     <section>
+        <div class ="circle"></div>
         <header>
-            <a href="#" class="logo"><img src="../source/img/home/logo.png"></a>
+            <a href="#" ><img src="../source/img/home/logo.png" class="logo"></a>
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
         </header>
-    </section>
-
-    <a href="login.php">Login</a>
+        
 
     
     
 </body>
 </html>
+
+
 
 
 
