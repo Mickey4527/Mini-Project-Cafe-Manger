99bf11a9ac41575272f1f4029c6c80b73ff72dc0
thanawat88 <toon1072@hotmail.com>
2023-10-28 03:09:43 +0700

    css cant run in homepage

--- views/home.php
+++ views/home.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="../assets/css/style-home.css">
 </head>
 <body>
-    <section>
+    <sction>
         <div class ="circle"></div>
         <header>
             <a href="#" ><img src="../source/img/home/logo.png" class="logo"></a>
@@ -22,14 +22,52 @@
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
         </header>
-        
+        <div class="content">
+            <div class="textBox">
+                <h2>Coffee shop Management system<br>It's <span>SEED</span></h2>
+                <p>It is a product management system for coffee shops, such as adding products. Delete product Edit product You can also record product sales information. Helps to know sales information and create excellent sales strategies.</p>
+                <a href="login.php">Login</a>
+            </div>
+            <div class="imgBox">
+                <img src="../source/img/home/img3.png" class="Amazon">
+            </div>
+        </div>
+        <ul class="thumb">
+            <li><img src="../source/img/home/thumb1.png" onclick="imgSlider('../source/img/home/img3.png');changeCircleColor('#017143');changeTextColor('#017143')"></li>
+            <li><img src="../source/img/home/thumb2.png" onclick="imgSlider('../source/img/home/img2.png');changeCircleColor('#B15A2E');changeTextColor('#B15A2E')"></li>
+            <li><img src="../source/img/home/thumb3.png" onclick="imgSlider('../source/img/home/img1.png');changeCircleColor('#665149');changeTextColor('#665149')"></li>
+        </ul>
+        <ul class="sci">
+            <li><a href="#"><img src="../source/img/home/facebook.png"></a></li>
+            <li><a href="#"><img src="../source/img/home/instagram.png"></a></li>
+            <li><a href="#"><img src="../source/img/home/twitter.png"></a></li>
+            
+        </ul>
+    </section>
+
+    <script type="text/javascript">
+        function imgSlider(anything){
+            document.querySelector('.Amazon').src = anything;
+        }
+
+        function changeCircleColor(color){
+            const circle = document.querySelector('.circle');
+            circle.style.background = color;
+        }
+        function changeTextColor(color){
+            const content = document.querySelector('.content .textBox a');
+            circle.style.background = color;
+        }
+    </script>
 
     
     
 </body>
 </html>
 
-
+<?php
+    htmlFooter();
+?>
 
 
 
