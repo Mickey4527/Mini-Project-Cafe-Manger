3c04a7c61336fad376042820078569f287524450
thanawat88 <toon1072@hotmail.com>
2023-10-31 01:03:59 +0700

    deleteReport

---
+++ global/product/report_sale.php
@@ -0,0 +1,27 @@
+<?php
+    include_once '../conn.php';
+    include_once '../function.php';
+    include_once '../layout.php';
+
+
+    if(isset($_POST['Saledelete'])){
+        if(empty($_POST['Saledelete'])){
+            toast('ไม่มีค่าข้อมูล','text-success','check-circle-fill');
+            exit();
+        }
+        else{
+            $reportId = $_POST['Saledelete'];
+            $result = deleteAnySql($conn,'history_sales','sale_id',$reportId);
+            if($result['status']){
+                toast('ลบข้อมูลเรียบร้อย','text-success','check-circle-fill');
+                exit();
+            }
+            else{
+              if($result['error'] === 1451){
+                toast('ไม่สามารถลบข้อมูลได้ เนื่องจากมีการใช้งานอยู่','text-danger','exclamation-triangle-fill');
+              }
+              exit();
+            }
+        }
+      }
+?>
--- views/report_sale.php
+++ views/report_sale.php
@@ -12,13 +12,42 @@
     htmlHeader('รายงานการขาย',null,'d-flex bg-cafe-white');
     navbar();
 
-    $result = getAnySql($conn,'user_id,first_name,last_name,email,telephone,creation_date','employees_account','roles','employee');
+    //querySql($conn,'SELECT * FROM history_sales')
+    $result = getAllSql($conn,'sale_id,trace_id,product_id,sale_date,amount,price','history_sales')
 ?>
 
 <div class="container-fluid p-5">
     <div class="col-12 d-flex justify-content-between align-items-center">
-        <h1 class="h3">รายงานการขาย</h1>
+        <div class="row">
+                <div class="col-12 my-3 d-flex justify-content-between align-items-center">
+                </div>
+                <div class="col-12">
+                    <div id="table">
+                        <?php table($result,'รายงานการขาย','sale_id',['รหัส','รหัสสินค้า','วันขาย','จำนวน','ราคา'],['trace_id','product_id','sale_date','amount','price']);?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<div class="modal" id="Delete" tabindex="-1" aria-labelledby="DeleteLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content rounded-3 shadow">
+        <div class="modal-body p-4 text-center">
+            <h5 class="mb-0">ต้องการลบข้อมูลหรือไม่ ?</h5>
+            <p class="mb-0">เมื่อลบข้อมูลแล้ว จะไม่สามารถกู้คืนกลับมาได้</p>
+        </div>
+        <div class="modal-footer flex-nowrap p-0">
+            <button type="button" class="btn btn-lg btn-link fs-6 text-decoration-none col-6 py-3 m-0 rounded-0 border-end" id="confirm"><strong class="text-danger">ลบการขาย</strong></button>
+            <button type="button" class="btn btn-lg btn-link fs-6 text-decoration-none col-6 py-3 m-0 rounded-0" data-bs-dismiss="modal">ยกเลิก</button>
+        </div>
+    </div>
+  </div>
+</div>
 
 
+<div id="toast"></div>
+<?php
+    htmlFooter(jsOut(['../assets/js/report_sale.js']));
+?>
 
 
